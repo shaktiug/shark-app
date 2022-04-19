adb148c5f86a853662f580a6a44307b2de39a14d
root <root@localhost.localdomain>
2022-04-19 12:39:09 +0530

    final

--- dev.sh
+++ dev.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Counting the number of lines in a list of files
 # function version
-IMAGE='REGNAME/$REPO:v${BUILD_NUMBER}' 
+IMAGE=$REGNAME/$REPO:v${BUILD_NUMBER} 
 echo "building image"
 docker build 
 docker build . -t $IMAGE
@@ -17,7 +17,7 @@ az webapp start --name $WEBAPP --resource-group $RS
 elif [ $choice == qa ]
 then
 az webapp stop --name $DEVAPP --resource-group $RS
-az webapp config container set --docker-custom-image-name $IMAGE  --docker-registry-server-password $REGPAS --docker-registry-server-url $REGADD --docker-registry-server-user $DID --name $WEBAPP --resource-group $RS
+az webapp config container set --docker-custom-image-name $IMAGE  --docker-registry-server-password $REGPAS --docker-registry-server-url $REGADD --docker-registry-server-user $DID --name $DEVAPP --resource-group $RS
 az webapp start --name $DEVAPP --resource-group $RS
 
 fi
