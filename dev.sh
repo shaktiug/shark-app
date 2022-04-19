3beb5e36c3c77692549ead48fb31f6f57c2ef055
root <root@localhost.localdomain>
2022-04-19 12:27:00 +0530

    new

--- dev.sh
+++ dev.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 # Counting the number of lines in a list of files
 # function version
+IMAGE='REGNAME/$REPO:v${BUILD_NUMBER}' 
 echo "building image"
 docker build 
-docker build . -t $REGNAME/$REPO:v${BUILD_NUMBER}
+docker build . -t $IMAGE
 echo "pushing image"
-docker push $REGADD/$REPO:v${BUILD_NUMBER} 
+docker push $IMAGE 
 
 if [ $choice == dev ]
 then
 az webapp stop --name $WEBAPP --resource-group $RS
-az webapp config container set --docker-custom-image-name $REGNAME/$REPO:v${BUILD_NUMBER}  --docker-registry-server-password $REGPAS --docker-registry-server-url $REGADD --docker-registry-server-user $DID --name $WEBAPP --resource-group $RS
+az webapp config container set --docker-custom-image-name $IMAGE  --docker-registry-server-password $REGPAS --docker-registry-server-url $REGADD --docker-registry-server-user $DID --name $WEBAPP --resource-group $RS
 az webapp start --name $WEBAPP --resource-group $RS
 
 elif [ $choice == qa ]
 then
 az webapp stop --name $DEVAPP --resource-group $RS
-az webapp config container set --docker-custom-image-name $REGNAME/$REPO:v${BUILD_NUMBER}  --docker-registry-server-password $REGPAS --docker-registry-server-url $REGADD --docker-registry-server-user $DID --name $WEBAPP --resource-group $RS
+az webapp config container set --docker-custom-image-name $IMAGE  --docker-registry-server-password $REGPAS --docker-registry-server-url $REGADD --docker-registry-server-user $DID --name $WEBAPP --resource-group $RS
 az webapp start --name $DEVAPP --resource-group $RS
 
 fi
