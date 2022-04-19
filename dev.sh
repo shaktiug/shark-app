e482e8d4c2bf17e3ac22b02a75b8ce5e81779983
root <root@localhost.localdomain>
2022-04-19 10:10:28 +0530

    finalone

--- dev.sh
+++ dev.sh
@@ -9,14 +9,15 @@ docker push dineshtestacr.azurecr.io/dineshtestrepo:v1
 
 if [ $choice == dev ]
 then
-az webapp stop --name dineshtest --resource-group dinesh-sg
-az webapp config container set --docker-custom-image-name dineshtestacr.azurecr.io/dineshtestrepo:v1  --docker-registry-server-password jp1ISYfYBGHGbGE=MIbDpOEr6UwdAr/n --docker-registry-server-url https://dineshtestacr.azurecr.io --docker-registry-server-user DockerUserId --name dineshtest --resource-group dinesh-sg
-az webapp start --name dineshtest --resource-group dinesh-sg
+az webapp stop --name $WEBAPP --resource-group $RS
+az webapp config container set --docker-custom-image-name dineshtestacr.azurecr.io/dineshtestrepo:v${BUILD_NUMBER}  --docker-registry-server-password $REGPAS --docker-registry-server-url https://dineshtestacr.azurecr.io --docker-registry-server-user dineshtestacr --name dineshtest --resource-group $RS
+az webapp start --name $WEBAPP --resource-group $RS
 
 elif [ $choice == qa ]
 then
-az webapp stop --name dineshtest1 --resource-group dinesh-sg
-az webapp config container set --docker-custom-image-name dineshtestacr.azurecr.io/dineshtestrepo:v1  --docker-registry-server-password jp1ISYfYBGHGbGE=MIbDpOEr6UwdAr/n --docker-registry-server-url https://dineshtestacr.azurecr.io --docker-registry-server-user DockerUserId --name dineshtest1 --resource-group dinesh-sg
-az webapp start --name dineshtest1 --resource-group dinesh-sg
+az webapp stop --name $DEVAPP --resource-group $RS
+az webapp config container set --docker-custom-image-name dineshtestacr.azurecr.io/dineshtestrepo:v${BUILD_NUMBER}  --docker-registry-server-password $REGPAS --docker-registry-server-url https://dineshtestacr.azurecr.io --docker-registry-server-user dineshtestacr --name $WEBAPP --resource-group $RS
+az webapp start --name $DEVAPP --resource-group $RS
 
 fi
+
