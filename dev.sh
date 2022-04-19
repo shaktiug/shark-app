d50dcd269fceed8cd89cd72cdd2e72d20aacfa66
root <root@localhost.localdomain>
2022-04-19 10:31:28 +0530

    new

--- dev.sh
+++ dev.sh
@@ -10,7 +10,7 @@ docker push dineshtestacr.azurecr.io/dineshtestrepo:v1
 if [ $choice == dev ]
 then
 az webapp stop --name $WEBAPP --resource-group $RS
-az webapp config container set --docker-custom-image-name $REGNAME:v${BUILD_NUMBER}  --docker-registry-server-password $REGPAS --docker-registry-server-url $REGADD --docker-registry-server-user $DID --name dineshtest --resource-group $RS
+az webapp config container set --docker-custom-image-name $REGNAME:v${BUILD_NUMBER}  --docker-registry-server-password $REGPAS --docker-registry-server-url $REGADD --docker-registry-server-user $DID --name $WEBAPP --resource-group $RS
 az webapp start --name $WEBAPP --resource-group $RS
 
 elif [ $choice == qa ]
