2ae29c926de1846502e104960bfa1f924df0e1c4
root <root@localhost.localdomain>
2022-04-19 10:40:09 +0530

    kar

--- dev.sh
+++ dev.sh
@@ -10,13 +10,13 @@ docker push dineshtestacr.azurecr.io/dineshtestrepo:v1
 if [ $choice == dev ]
 then
 az webapp stop --name $WEBAPP --resource-group $RS
-az webapp config container set --docker-custom-image-name $REGNAME:v${BUILD_NUMBER}  --docker-registry-server-password $REGPAS --docker-registry-server-url $REGADD --docker-registry-server-user $DID --name $WEBAPP --resource-group $RS
+az webapp config container set --docker-custom-image-name $REGNAME/$REPO:v${BUILD_NUMBER}  --docker-registry-server-password $REGPAS --docker-registry-server-url $REGADD --docker-registry-server-user $DID --name $WEBAPP --resource-group $RS
 az webapp start --name $WEBAPP --resource-group $RS
 
 elif [ $choice == qa ]
 then
 az webapp stop --name $DEVAPP --resource-group $RS
-az webapp config container set --docker-custom-image-name $REGNAME:v${BUILD_NUMBER}  --docker-registry-server-password $REGPAS --docker-registry-server-url $REGADD --docker-registry-server-user $DID --name $WEBAPP --resource-group $RS
+az webapp config container set --docker-custom-image-name $REGNAME/$REPO:v${BUILD_NUMBER}  --docker-registry-server-password $REGPAS --docker-registry-server-url $REGADD --docker-registry-server-user $DID --name $WEBAPP --resource-group $RS
 az webapp start --name $DEVAPP --resource-group $RS
 
 fi
